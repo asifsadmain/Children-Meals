649a7166a3a38205c798cc38539bfb001fac56e9
asif2478 <sristy2478@gmail.com>
2018-04-21 21:01:50 +0600

    First commit

---
+++ src/ChildrenMeals.java
@@ -0,0 +1,42 @@
+public class ChildrenMeals
+{
+    private String mainItem;
+    private String sideItem;
+    private String drink;
+    private String toy;
+
+    //setters
+    public void setMainItem(String mainItem) {
+        this.mainItem = mainItem;
+    }
+
+    public void setSideItem(String sideItem) {
+        this.sideItem = sideItem;
+    }
+
+    public void setDrink(String drink) {
+        this.drink = drink;
+    }
+
+    public void setToy(String toy) {
+        this.toy = toy;
+    }
+
+    //getters
+
+    public String getMainItem() {
+        return mainItem;
+    }
+
+    public String getSideItem() {
+        return sideItem;
+    }
+
+    public String getDrink() {
+        return drink;
+    }
+
+    public String getToy() {
+        return toy;
+    }
+}
